213ac2906afa369146dc62502f5255eab48b0826
Braid-Grizzell, Ezri <ebraidgrizzell@butler.edu>
2025-02-10 13:18:46 -0500

    working styles

--- src/components/CreatureCard/StatHeader/StatHeader.jsx
+++ src/components/CreatureCard/StatHeader/StatHeader.jsx
@@ -1,46 +1,27 @@
 import { Grid, Typography } from "@mui/material";
-import './StatHeader.scss'
+// import './StatHeader.scss'
 import BlockDivider from "../BlockDivider/BlockDivider";
 
-const StatHeader = ({ currentMonster }) => {
-    // TODO: implement list of speeds
+const StatHeader = ({
+    currentMonster
+}) => {
+    // className="monster-grid-container"
     return (
         <div className={'red'}>
             <Typography className={'card-name'} variant="h5">{currentMonster?.name}</Typography>
             <Typography className={'description'}>{currentMonster?.size} {currentMonster?.type}, {currentMonster?.alignment}</Typography>
             <BlockDivider />
-            <Grid container spacing={1}>
+            <Grid container className="monster-inline-content-block">
                 <Grid item>
                     <Typography fontWeight={'bold'}>Armor Class</Typography>
+                    {/* <Typography>{currentMonster?.armor_class}</Typography> */}
                 </Grid>
-                <Grid item xs={2}>
-                    <Typography>{currentMonster?.armor_class?.value}</Typography>
-                </Grid>
-            </Grid>
-            <Grid container spacing={1}>
-                <Grid item>
+                <Grid item >
                     <Typography fontWeight={'bold'}>Hit Points</Typography>
-                </Grid>
-                <Grid item xs={2}>
                     <Typography>{currentMonster?.hit_points} ({currentMonster?.hit_dice})</Typography>
                 </Grid>
-            </Grid>
-            <Grid container spacing={1}>
                 <Grid item>
                     <Typography fontWeight={'bold'}>Speed</Typography>
-                </Grid>
-                <Grid item>
-                    {/* <div className="grid-item-list">
-                        {Object.entries(currentMonster?.speed).map((key, value) => {
-
-                            if (value !== null) {
-                                return (
-                                    <Typography>`${value}, `</Typography>
-                                )
-                            }
-
-                        })}
-                    </div> */}
                 </Grid>
             </Grid>
         </div>
